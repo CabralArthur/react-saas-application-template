b7f0654e60f6caf31ed3e619f37cb55e1c58a114
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2025-01-30 03:40:23 -0300

    Enhance PrivateRouteGuard with root path redirection

--- src/guards/Private/Private.tsx
+++ src/guards/Private/Private.tsx
@@ -1,11 +1,17 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+
 import useIsLogged from "@/hooks/useIsLogged";
-import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRouteGuard() {
   const isLogged = useIsLogged();
+  const { pathname } = useLocation();
 
   if (!isLogged) {
     return <Navigate to="/login" replace />;
+  }
+
+  if (pathname === "/") {
+    return <Navigate to="/home" replace />;
   }
 
   return <Outlet />;
